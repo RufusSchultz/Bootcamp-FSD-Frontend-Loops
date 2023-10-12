edaf52eab75196220de9a2117dbad96a65d83653
RufusSchultz <rufusschultz@gmail.com>
2023-10-12 13:27:46 +0200

    Finished loops-and-arrays.js

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -12,9 +12,11 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
-
-
+for (let i = 0; i < names.length; i++) {
+    names[i] = names[i] + "je";
+    console.log(names[i]);
+}
+console.log("-----------------------------");
 
 // ==========================================
 // Opdracht 2
@@ -30,9 +32,15 @@ const numbers = [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
-
-
-
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        numbers[i] = numbers[i] * 2;
+    } else {
+        numbers[i] = numbers[i] * 3;
+    }
+    console.log(numbers[i])
+}
+console.log("-----------------------------");
 
 // ==========================================
 // Opdracht 3
@@ -57,5 +65,12 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 11 is 1331'
 // ]
 
+let output = "";
+let squaresCubed = 0;
 
+for (let i = 0; i < squares.length; i++) {
+    squaresCubed = squares[i] * squares[i] * squares [i];
+    output = `Het volume van ${squares[i]} is ${squaresCubed}`;
+    console.log(output);
+}
 
